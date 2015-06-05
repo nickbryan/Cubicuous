8143346789e0d1bf45cb5484e5b914926c79f6ab
maxmememax <maxting@hotmail.co.uk>
2015-06-05 12:09:02 +0100

    Added categorized test

---
+++ test/window/main.cpp
@@ -0,0 +1,19 @@
+#include "../../src/Window/Window.h"
+
+using Cubicuous::Debugging::Logger;
+
+int main() {
+	Cubicuous::Window::Window window("Cubicuous", 800, 600);
+
+	Logger::log("Renderer: " + Logger::toLoggable(glGetString(GL_RENDERER)));
+	Logger::log("OpenGl Version Supported: " + Logger::toLoggable(glGetString(GL_VERSION)));
+	Logger::log("GLSL version: " + Logger::toLoggable(glGetString(GL_SHADING_LANGUAGE_VERSION)));
+	Logger::log("Vendor: " + Logger::toLoggable(glGetString(GL_VENDOR)));
+
+	while (window.isOpen()) {
+		window.clear();
+		window.update();
+	}
+
+	return 0;
+}
