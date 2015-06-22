0ad83ead93b046442b4ea9699a6840a72853c335
maxmememax <maxting@hotmail.co.uk>
2015-06-22 22:43:32 +0100

    Quick push for segfault replication

--- test/Game/TestScene.cpp
+++ test/Game/TestScene.cpp
@@ -7,8 +7,10 @@
 void TestScene::render(double deltaTime) {
     Cubicuous::Graphics::ShaderProgram *shaderProgram = this->_game->getShaderProgram();
     shaderProgram->enable();
-    shaderProgram->setVertexAttribArray("position", 3, GL_FLOAT, GL_FALSE, 0, 0);
+    shaderProgram->setVertexAttribArray("position", 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
     this->_cubeBuffer.updateData(this->_cube);
+
     shaderProgram->setUniformMat4("projection", glm::perspective(glm::radians(45.0f), ((float)800 / (float)600), 1.0f, 10.0f));
     shaderProgram->setUniformMat4("view", glm::lookAt(
             glm::vec3(1.5f, 1.5f, 1.5f),
@@ -17,8 +19,8 @@ void TestScene::render(double deltaTime) {
     ));
     shaderProgram->setUniformMat4("model", glm::mat4(1.0f));
 
-    shaderProgram->disableVertexAttribArray();
     glDrawArrays(GL_TRIANGLES, 0, 36);
+    shaderProgram->disableVertexAttribArray(); //SEGFAULT
 }
 
 void TestScene::update() {
--- test/Game/TestScene.h
+++ test/Game/TestScene.h
@@ -7,37 +7,55 @@
 
 class TestScene : public Cubicuous::Core::Scene {
 private:
-    GLfloat _cube[117] = {
-            // back
-            -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, 0.5f, -0.5f,
-            0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, -0.5f, -0.5f,
+    GLfloat _cube[108] = {//X, Y, Z
+            -0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f,  0.5f, -0.5f,
+            0.5f,  0.5f, -0.5f,
+            -0.5f,  0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
 
-            // front
-            -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
-            0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, -0.5f, 0.5f,
+            -0.5f, -0.5f,  0.5f,
+            0.5f, -0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
+            -0.5f, -0.5f,  0.5f,
 
-            // left
-            -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, -0.5f,
-            -0.5f, -0.5f, -0.5f, -0.5f, -0.5f, -0.5f,
-            -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,
+            -0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            -0.5f, -0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
 
-            // right
-            0.5f, 0.5f, 0.5f, 0.5f, 0.5f, -0.5f, 0.5f, -0.5f, -0.5f,
-            0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f, 0.5f,
+            0.5f,  0.5f,  0.5f,
+            0.5f,  0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
 
-            // bottom
-            -0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, 0.5f,
-            0.5f, -0.5f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f, -0.5f, -0.5f,
+            -0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f, -0.5f,
+            0.5f, -0.5f,  0.5f,
+            0.5f, -0.5f,  0.5f,
+            -0.5f, -0.5f,  0.5f,
+            -0.5f, -0.5f, -0.5f,
 
-            // top
-            -0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, 0.5f,
-            0.5f, 0.5f, 0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f, -0.5f
+            -0.5f,  0.5f, -0.5f,
+            0.5f,  0.5f, -0.5f,
+            0.5f,  0.5f,  0.5f,
+            0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f,  0.5f,
+            -0.5f,  0.5f, -0.5f
     };
 
     VertexBuffer& _cubeBuffer;
 
 public:
-    inline TestScene(Cubicuous::Game* game) : Scene(game), _cubeBuffer(game->getVertexBuffer("cubeBuffer")) { };
+    inline TestScene(Cubicuous::Game* game) : Scene(game), _cubeBuffer(game->getVertexBuffer("cubeBuffer")) {
+    };
 
     void render(double deltaTime) override;
 
